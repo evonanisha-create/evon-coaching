a0439b19ba9b1096fa8c753d668f33e8ee5da67f
evonanisha-create <evonanisha@gmail.com>
2026-01-13 20:46:52 -0600

    Update tailwind.config.ts
    Changed darker grey to lighter grey (still mapped to blue)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         'brand': {
           black: '#000000',
           white: '#FFFFFF',
-          blue: '#333333',        // Darker Grey
+          blue: '#444444',        // Lighter Grey
           pink: '#D90166',        // Dark Hot Pink
         },
         // Extended palette for UI
